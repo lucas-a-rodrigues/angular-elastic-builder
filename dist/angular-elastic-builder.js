f6cbddb4378decf0596c5d0f34d9a72811439c72
Lucas.Rodrigues <lucas.rodrigues@stf.jus.br>
2016-03-10 17:08:53 -0300

    Update build

--- dist/angular-elastic-builder.js
+++ dist/angular-elastic-builder.js
@@ -2,10 +2,10 @@
  * # angular-elastic-builder
  * ## Angular Module for building an Elasticsearch Query
  *
- * @version v1.4.0
- * @link https://github.com/dncrews/angular-elastic-builder.git
+ * @version v1.5.0
+ * @link https://github.com/lucas-a-rodrigues/angular-elastic-builder.git
  * @license MIT
- * @author Dan Crews <crewsd@gmail.com>
+ * @author Dan Crews <crewsd@gmail.com>, Lucas Rodrigues <lucasmoc@gmail.com>
  */
 
 /**
@@ -22,6 +22,326 @@
   angular.module('angular-elastic-builder', [
     'RecursionHelper',
   ]);
+
+})(window.angular);
+
+/**
+ * angular-elastic-builder
+ *
+ * /src/services/GroupClassHelper.js
+ *
+ * This keeps all of the groups colored correctly
+ */
+
+(function(angular) {
+  'use strict';
+
+  angular.module('angular-elastic-builder')
+    .factory('groupClassHelper', function groupClassHelper() {
+
+      return function(level) {
+        var levels = [
+          '',
+          'list-group-item-info',
+          'list-group-item-success',
+          'list-group-item-warning',
+          'list-group-item-danger',
+        ];
+
+        return levels[level % levels.length];
+      };
+    });
+
+})(window.angular);
+
+/**
+ * angular-elastic-builder
+ *
+ * /src/services/QueryService.js
+ *
+ * This file is used to convert filters into queries, and vice versa
+ */
+
+(function(angular) {
+	'use strict';
+
+	angular.module('angular-elastic-builder').factory('elasticQueryService', [ function() {
+
+		return {
+			toFilters : toFilters,
+			toQuery : toQuery,
+		};
+	} ]);
+
+	function toFilters(query, fieldMap) {
+		var filters = query.map(parseQueryGroup.bind(query, fieldMap));
+		return filters;
+	}
+
+	function toQuery(filters, fieldMap) {
+		var query = filters.map(parseFilterGroup.bind(filters, fieldMap)).filter(function(item) {
+			return !!item;
+		});
+		return query;
+	}
+
+	function parseQueryGroup(fieldMap, group, truthy) {
+		if (truthy !== false)
+			truthy = true;
+
+		var key = Object.keys(group)[0], typeMap = {
+			or : 'group',
+			and : 'group',
+			range : 'number',
+		}, type = typeMap[key] || 'item', obj = getFilterTemplate(type);
+
+		switch (key) {
+			case 'or':
+			case 'and':
+				obj.rules = group[key].map(parseQueryGroup.bind(group, fieldMap));
+				obj.subType = key;
+				break;
+				
+			case 'missing':
+			case 'exists':
+				obj.field = group[key].field;
+				obj.subType = { exists : 'exists', missing : 'notExists' }[key];
+				delete obj.value;
+				break;
+				
+			case 'term':
+			case 'terms':
+			case 'match':
+			case 'match_phrase_prefix':
+				obj.field = Object.keys(group[key])[0];
+				var fieldData = fieldMap[Object.keys(group[key])[0]];
+				obj.subType = truthy ? 'equals' : 'notEquals';
+				obj.value = group[key][obj.field];
+				break;
+				
+			case 'range':
+				obj.field = Object.keys(group[key])[0];
+				
+				if (angular.isNumber(group[key][obj.field][obj.subType])) {
+					obj.subType = Object.keys(group[key][obj.field])[0];
+					obj.value = group[key][obj.field][obj.subType];
+					
+				} else if (angular.isDefined(Object.keys(group[key][obj.field])[1])) {
+					var date = group[key][obj.field]['gte'];
+					
+					if (date.indexOf('now-') > -1) {
+						obj.subType = 'last';
+						obj.value = parseInt(date.split('now-')[1].split('d')[0]);
+					} else if (date.indexOf('now') > -1) {
+						obj.subType = 'next';
+						date = group[key][obj.field]['lte'];
+						obj.value = parseInt(date.split('now+')[1].split('d')[0]);
+					} else {
+						obj.subType = 'equals';
+						var parts = date.split(' ')[0].split('-');
+						obj.date = parts[2] + '/' + parts[1] + '/' + parts[0];
+					}
+				} else {
+					obj.subType = Object.keys(group[key][obj.field])[0];
+					
+					var date = group[key][obj.field][obj.subType];
+					var parts = date.split(' ')[0].split('-');
+					obj.date = parts[2] + '/' + parts[1] + '/' + parts[0];
+				}
+				break;
+				
+			case 'not':
+				obj = parseQueryGroup(fieldMap, group[key].filter, false);
+				break;
+				
+			default:
+				obj.field = Object.keys(group[key])[0];
+				break;
+		}
+		return obj;
+	}
+
+	function parseFilterGroup(fieldMap, group) {
+		var obj = {};
+		if (group.type === 'group') {
+			obj[group.subType] = group.rules.map(parseFilterGroup.bind(group, fieldMap)).filter(function(item) {
+				return !!item;
+			});
+			return obj;
+		}
+
+		var fieldName = group.field;
+		var fieldData = fieldMap[fieldName];
+
+		if (!fieldName || !group.subType) return;
+		
+		//Common subTypes
+		switch (group.subType) {
+			
+			case 'exists':
+				obj.exists = { field : fieldName };
+				break;
+			case 'notExists':
+				obj.missing = { field : fieldName };
+				break;
+			case 'equals':
+				if (fieldData.type === 'date') break;
+				if (isUndefinedOrNull(group.value)) return;
+				
+				if (angular.isArray(group.value)) {
+					obj.terms = {};
+					obj.terms[fieldName] = group.value;
+				} else {
+					obj.term = {};
+					obj.term[fieldName] = group.value;
+				}
+				break;
+			case 'notEquals':
+				if (isUndefinedOrNull(group.value)) return;
+				
+				if (angular.isArray(group.value)) {
+					obj.not = { filter : { terms : {}}};
+					obj.not.filter.terms[fieldName] = group.value;
+				} else {
+					obj.not = { filter : { term : {}}};
+					obj.not.filter.term[fieldName] = group.value;
+				}
+				break;	
+			
+		}
+
+		//Specific type rule
+		switch (fieldData.type) {
+			
+			case 'boolean':			
+			case 'term':				
+				switch (group.subType) {
+					case 'prefix':
+						if (isUndefinedOrNull(group.value)) return;
+						
+						obj.match_phrase_prefix = {};
+						obj.match_phrase_prefix[fieldName] = group.value;
+						break;
+					case 'contains':
+						if (isUndefinedOrNull(group.value)) return;
+						
+						obj.match = {};
+						obj.match[fieldName] = group.value;
+						break;
+				}
+				break;
+
+			case 'number':
+				if (isNotNumber(group.value)) return;
+				if (group.subType === 'equals' || group.subType === 'notEquals') break; 
+				
+				obj.range = {};
+				obj.range[fieldName] = {};
+				obj.range[fieldName][group.subType] = group.value;
+				break;
+
+			case 'date':
+				switch(group.subType) {
+					case 'last':
+						if (isNotNumber(group.value)) return;
+						
+						obj.range = {};
+						obj.range[fieldName] = {};
+						obj.range[fieldName]['gte'] = 'now-' + group.value + 'd';
+						obj.range[fieldName]['lte'] = 'now';
+						break;
+					
+					case 'next':
+						if (isNotNumber(group.value)) return;
+						
+						obj.range = {};
+						obj.range[fieldName] = {};
+						obj.range[fieldName]['gte'] = 'now';
+						obj.range[fieldName]['lte'] = 'now+' + group.value + 'd';
+						break;
+					
+					case 'equals':
+						var parsed = validateDate(group.date);
+						if (!parsed) return;
+							
+						obj.range = {};
+						obj.range[fieldName] = {};
+						obj.range[fieldName]['gte'] = parsed + ' 00:00:00';
+						obj.range[fieldName]['lte'] = parsed + ' 23:59:59';
+						break;
+						
+					case 'gt':
+					case 'gte':
+						var parsed = validateDate(group.date);
+						if (!parsed) return;
+						
+						obj.range = {};
+						obj.range[fieldName] = {};
+						obj.range[fieldName][group.subType] = parsed + ' 00:00:00';
+						break;
+					
+					case 'lt':
+					case 'lte':
+						var parsed = validateDate(group.date);
+						if (!parsed) return;
+						
+						obj.range = {};
+						obj.range[fieldName] = {};
+						obj.range[fieldName][group.subType] = parsed + ' 23:59:59';
+						break;
+				}
+				break;
+				
+			case 'multi':
+				break;
+				
+			default:
+				throw new Error('Unexpected type');
+		}
+		return obj;
+	}
+
+	function getFilterTemplate(type) {
+		var templates = {
+			group : {
+				type : 'group',
+				subType : '',
+				rules : [],
+			},
+			item : {
+				field : '',
+				subType : '',
+				value : '',
+			},
+			number : {
+				field : '',
+				subType : '',
+				value : null,
+			}
+		};
+
+		return angular.copy(templates[type]);
+	}
+	
+	function isNotNumber(value) {
+		return value === undefined || value === null || Number.isNaN(value);
+	}
+	
+	function isUndefinedOrNull(value) {
+		return value === undefined || value === null;
+	}
+	
+	function validateDate(date) {
+		if (!angular.isString(date)) return false;
+		
+		var parts = date.split("/");
+		var formatedDate = parts[2] + '-' + parts[1] + '-' + parts[0];
+		var time = Date.parse(formatedDate);
+		
+		if (Number.isNaN(time)) return false;
+
+		return formatedDate;
+	}
 
 })(window.angular);
 
@@ -360,326 +680,6 @@
 	}
 
 	]);
-
-})(window.angular);
-
-/**
- * angular-elastic-builder
- *
- * /src/services/GroupClassHelper.js
- *
- * This keeps all of the groups colored correctly
- */
-
-(function(angular) {
-  'use strict';
-
-  angular.module('angular-elastic-builder')
-    .factory('groupClassHelper', function groupClassHelper() {
-
-      return function(level) {
-        var levels = [
-          '',
-          'list-group-item-info',
-          'list-group-item-success',
-          'list-group-item-warning',
-          'list-group-item-danger',
-        ];
-
-        return levels[level % levels.length];
-      };
-    });
-
-})(window.angular);
-
-/**
- * angular-elastic-builder
- *
- * /src/services/QueryService.js
- *
- * This file is used to convert filters into queries, and vice versa
- */
-
-(function(angular) {
-	'use strict';
-
-	angular.module('angular-elastic-builder').factory('elasticQueryService', [ function() {
-
-		return {
-			toFilters : toFilters,
-			toQuery : toQuery,
-		};
-	} ]);
-
-	function toFilters(query, fieldMap) {
-		var filters = query.map(parseQueryGroup.bind(query, fieldMap));
-		return filters;
-	}
-
-	function toQuery(filters, fieldMap) {
-		var query = filters.map(parseFilterGroup.bind(filters, fieldMap)).filter(function(item) {
-			return !!item;
-		});
-		return query;
-	}
-
-	function parseQueryGroup(fieldMap, group, truthy) {
-		if (truthy !== false)
-			truthy = true;
-
-		var key = Object.keys(group)[0], typeMap = {
-			or : 'group',
-			and : 'group',
-			range : 'number',
-		}, type = typeMap[key] || 'item', obj = getFilterTemplate(type);
-
-		switch (key) {
-			case 'or':
-			case 'and':
-				obj.rules = group[key].map(parseQueryGroup.bind(group, fieldMap));
-				obj.subType = key;
-				break;
-				
-			case 'missing':
-			case 'exists':
-				obj.field = group[key].field;
-				obj.subType = { exists : 'exists', missing : 'notExists' }[key];
-				delete obj.value;
-				break;
-				
-			case 'term':
-			case 'terms':
-			case 'match':
-			case 'match_phrase_prefix':
-				obj.field = Object.keys(group[key])[0];
-				var fieldData = fieldMap[Object.keys(group[key])[0]];
-				obj.subType = truthy ? 'equals' : 'notEquals';
-				obj.value = group[key][obj.field];
-				break;
-				
-			case 'range':
-				obj.field = Object.keys(group[key])[0];
-				
-				if (angular.isNumber(group[key][obj.field][obj.subType])) {
-					obj.subType = Object.keys(group[key][obj.field])[0];
-					obj.value = group[key][obj.field][obj.subType];
-					
-				} else if (angular.isDefined(Object.keys(group[key][obj.field])[1])) {
-					var date = group[key][obj.field]['gte'];
-					
-					if (date.indexOf('now-') > -1) {
-						obj.subType = 'last';
-						obj.value = parseInt(date.split('now-')[1].split('d')[0]);
-					} else if (date.indexOf('now') > -1) {
-						obj.subType = 'next';
-						date = group[key][obj.field]['lte'];
-						obj.value = parseInt(date.split('now+')[1].split('d')[0]);
-					} else {
-						obj.subType = 'equals';
-						var parts = date.split(' ')[0].split('-');
-						obj.date = parts[2] + '/' + parts[1] + '/' + parts[0];
-					}
-				} else {
-					obj.subType = Object.keys(group[key][obj.field])[0];
-					
-					var date = group[key][obj.field][obj.subType];
-					var parts = date.split(' ')[0].split('-');
-					obj.date = parts[2] + '/' + parts[1] + '/' + parts[0];
-				}
-				break;
-				
-			case 'not':
-				obj = parseQueryGroup(fieldMap, group[key].filter, false);
-				break;
-				
-			default:
-				obj.field = Object.keys(group[key])[0];
-				break;
-		}
-		return obj;
-	}
-
-	function parseFilterGroup(fieldMap, group) {
-		var obj = {};
-		if (group.type === 'group') {
-			obj[group.subType] = group.rules.map(parseFilterGroup.bind(group, fieldMap)).filter(function(item) {
-				return !!item;
-			});
-			return obj;
-		}
-
-		var fieldName = group.field;
-		var fieldData = fieldMap[fieldName];
-
-		if (!fieldName || !group.subType) return;
-		
-		//Common subTypes
-		switch (group.subType) {
-			
-			case 'exists':
-				obj.exists = { field : fieldName };
-				break;
-			case 'notExists':
-				obj.missing = { field : fieldName };
-				break;
-			case 'equals':
-				if (fieldData.type === 'date') break;
-				if (isUndefinedOrNull(group.value)) return;
-				
-				if (angular.isArray(group.value)) {
-					obj.terms = {};
-					obj.terms[fieldName] = group.value;
-				} else {
-					obj.term = {};
-					obj.term[fieldName] = group.value;
-				}
-				break;
-			case 'notEquals':
-				if (isUndefinedOrNull(group.value)) return;
-				
-				if (angular.isArray(group.value)) {
-					obj.not = { filter : { terms : {}}};
-					obj.not.filter.terms[fieldName] = group.value;
-				} else {
-					obj.not = { filter : { term : {}}};
-					obj.not.filter.term[fieldName] = group.value;
-				}
-				break;	
-			
-		}
-
-		//Specific type rule
-		switch (fieldData.type) {
-			
-			case 'boolean':			
-			case 'term':				
-				switch (group.subType) {
-					case 'prefix':
-						if (isUndefinedOrNull(group.value)) return;
-						
-						obj.match_phrase_prefix = {};
-						obj.match_phrase_prefix[fieldName] = group.value;
-						break;
-					case 'contains':
-						if (isUndefinedOrNull(group.value)) return;
-						
-						obj.match = {};
-						obj.match[fieldName] = group.value;
-						break;
-				}
-				break;
-
-			case 'number':
-				if (isNotNumber(group.value)) return;
-				if (group.subType === 'equals' || group.subType === 'notEquals') break; 
-				
-				obj.range = {};
-				obj.range[fieldName] = {};
-				obj.range[fieldName][group.subType] = group.value;
-				break;
-
-			case 'date':
-				switch(group.subType) {
-					case 'last':
-						if (isNotNumber(group.value)) return;
-						
-						obj.range = {};
-						obj.range[fieldName] = {};
-						obj.range[fieldName]['gte'] = 'now-' + group.value + 'd';
-						obj.range[fieldName]['lte'] = 'now';
-						break;
-					
-					case 'next':
-						if (isNotNumber(group.value)) return;
-						
-						obj.range = {};
-						obj.range[fieldName] = {};
-						obj.range[fieldName]['gte'] = 'now';
-						obj.range[fieldName]['lte'] = 'now+' + group.value + 'd';
-						break;
-					
-					case 'equals':
-						var parsed = validateDate(group.date);
-						if (!parsed) return;
-							
-						obj.range = {};
-						obj.range[fieldName] = {};
-						obj.range[fieldName]['gte'] = parsed + ' 00:00:00';
-						obj.range[fieldName]['lte'] = parsed + ' 23:59:59';
-						break;
-						
-					case 'gt':
-					case 'gte':
-						var parsed = validateDate(group.date);
-						if (!parsed) return;
-						
-						obj.range = {};
-						obj.range[fieldName] = {};
-						obj.range[fieldName][group.subType] = parsed + ' 00:00:00';
-						break;
-					
-					case 'lt':
-					case 'lte':
-						var parsed = validateDate(group.date);
-						if (!parsed) return;
-						
-						obj.range = {};
-						obj.range[fieldName] = {};
-						obj.range[fieldName][group.subType] = parsed + ' 23:59:59';
-						break;
-				}
-				break;
-				
-			case 'multi':
-				break;
-				
-			default:
-				throw new Error('Unexpected type');
-		}
-		return obj;
-	}
-
-	function getFilterTemplate(type) {
-		var templates = {
-			group : {
-				type : 'group',
-				subType : '',
-				rules : [],
-			},
-			item : {
-				field : '',
-				subType : '',
-				value : '',
-			},
-			number : {
-				field : '',
-				subType : '',
-				value : null,
-			}
-		};
-
-		return angular.copy(templates[type]);
-	}
-	
-	function isNotNumber(value) {
-		return value === undefined || value === null || Number.isNaN(value);
-	}
-	
-	function isUndefinedOrNull(value) {
-		return value === undefined || value === null;
-	}
-	
-	function validateDate(date) {
-		if (!angular.isString(date)) return false;
-		
-		var parts = date.split("/");
-		var formatedDate = parts[2] + '-' + parts[1] + '-' + parts[0];
-		var time = Date.parse(formatedDate);
-		
-		if (Number.isNaN(time)) return false;
-
-		return formatedDate;
-	}
 
 })(window.angular);
 
